6005a8ebb6455dff61b7ba781b29c11969c97741
JeanneEricksonCooley <cool0020@gmail.com>
2015-04-28 15:58:11 -0500

    first commit

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-var apikey = ''; // Put your API key here
+var array = [];
+
+var apikey = '10d0a85137d94904c75e456dcdf91468227c09ca'; // Put your API key here
 
 
 // Use this function to do stuff with your results. 
 // It is called after 'search' is executed.
 function searchCallback(results) {
-    console.log(results);
+    for (var j = 0; j < 9; j++) {
+		console.log(results[j]);
+
+		$(".games").append("<div class='col-md-4 well'><button class='btn btn-small bob'>REMOVE</button><p>" + results[j].name + "</p><br> <p>" + results[j].deck + "</p><br><img src='" + results[j].image.medium_url + "'/></p></div>");
+	}
 }
 
-$(document).ready(function() {
+//function displayStuff(results) {
 
-	// Start the search here!
-	
-});
+//}
+
+
 
 // HELPER FUNCTION
 // Executes a search using 'query' and runs searchCallback on the results of a success.
@@ -32,3 +38,24 @@ function search(query){
 	});
 
 }
+
+	$(document).ready(function() {
+
+		// Start the search here!
+		search('batman');
+
+		$(".games").on("click", ".bob", function () {
+			$(this).parent().remove();
+		});
+	});
+
+/*
+<div class="games">
+	<div class="game">
+	...
+		<button class="btn btn-small bob"></button>
+	</div>
+</div>
+
+
+ */
